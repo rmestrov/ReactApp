03f11d5cd0c5c4915b43083024c7e7242ac84ae7
rmestrov <rmestrov@fesb.hr>
2018-01-24 02:33:02 +0100

    App shows 15 random pokemons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import FormContainer from './containers/FormContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      data:[],
+    }
+  }
+
+componentDidMount()
+{
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=15')
+  .then((Response) => Response.json())
+  .then((findresponse) =>
+  {
+      this.setState({
+        data:findresponse.results,
+      })
+  })
+}
+
   render() {
+    let i=0;
     return (
-     <div className="container">
-       <div classNmae="columns">
-          <div className="col-md-9 centered">
-            <h3>Form Component</h3>
-            <FormContainer />
+      <div class="list-group">
+      
+        {
+          this.state.data.map((dynamicData,key) =>
+          <div class="container">
+            <span>
+              <h2>{dynamicData.name}</h2>
+              Result item: {i += 1}
+            </span>
           </div>
-        </div>
+          )
+        }
+      
       </div>
     );
   }
